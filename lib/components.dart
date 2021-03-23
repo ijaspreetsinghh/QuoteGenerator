b0afbc3a59fbcde5996056d7ed9950babb12f003
Jaspreet Singh <jaspreet.codrity@gmail.com>
2021-03-23 09:16:57 +0530

    added test ad units for banner and Interstitial units

--- lib/components.dart
+++ lib/components.dart
@@ -94,98 +94,113 @@ class _MainContentViewBuilderState extends State<MainContentViewBuilder> {
     // Called when an ad is in the process of leaving the application.
     onApplicationExit: (Ad ad) => print('Left application.'),
   );
+  final InterstitialAd intAd = InterstitialAd(
+      adUnitId: InterstitialAd
+          .testAdUnitId, //'ca-app-pub-4325421502488315/2498349214'
+      listener: AdListener(),
+      request: AdRequest());
   void _modalBottomSheetMenu() {
     String _sharableContent = '"${widget.content}"\nBy - ${widget.author}';
     showModalBottomSheet(
         context: context,
         builder: (builder) {
-          return Container(
-              height: 300,
-              decoration: BoxDecoration(
-                color: Colors.white,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Container(
-                  //   padding: EdgeInsets.symmetric(
-                  //       horizontal: kHPadding * 1.5, vertical: kVPadding * 2),
-                  //   child: Text(
-                  //     'Share this on:',
-                  //     style: kAvertaTextStyle.copyWith(color: kPrimaryColor),
-                  //   ),
-                  // ),
-                  SizedBox(
-                    height: kVPadding * 3,
+          return Random().nextInt(6) == 4
+              ? intAd
+              : Container(
+                  height: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      // Container(
+                      //   padding: EdgeInsets.symmetric(
+                      //       horizontal: kHPadding * 1.5, vertical: kVPadding * 2),
+                      //   child: Text(
+                      //     'Share this on:',
+                      //     style: kAvertaTextStyle.copyWith(color: kPrimaryColor),
+                      //   ),
+                      // ),
+                      // SizedBox(
+                      //   height: kVPadding * 2,
+                      // ),
                       Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(
-                                color: kPrimaryColor.withOpacity(.4),
-                                width: 2)),
-                        child: IconButton(
-                            icon: Icon(
-                              Icons.copy_rounded,
-                              size: 30,
-                            ),
-                            onPressed: () {
-                              SocialShare.copyToClipboard(_sharableContent);
-                            }),
+                        margin: EdgeInsets.symmetric(vertical: kVPadding * 2),
+                        alignment: Alignment.center,
+                        child: AdWidget(ad: myBanner),
+                        width: myBanner.size.width.toDouble(),
+                        height: myBanner.size.height.toDouble(),
                       ),
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(
-                                color: kPrimaryColor.withOpacity(.4),
-                                width: 2)),
-                        child: IconButton(
-                            icon: Icon(
-                              Icons.share_rounded,
-                              size: 30,
-                            ),
-                            onPressed: () =>
-                                SocialShare.shareOptions(_sharableContent)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                border: Border.all(
+                                    color: kPrimaryColor.withOpacity(.4),
+                                    width: 2)),
+                            child: IconButton(
+                                icon: Icon(
+                                  Icons.copy_rounded,
+                                  size: 30,
+                                ),
+                                onPressed: () {
+                                  SocialShare.copyToClipboard(_sharableContent);
+                                }),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                border: Border.all(
+                                    color: kPrimaryColor.withOpacity(.4),
+                                    width: 2)),
+                            child: IconButton(
+                                icon: Icon(
+                                  Icons.share_rounded,
+                                  size: 30,
+                                ),
+                                onPressed: () =>
+                                    SocialShare.shareOptions(_sharableContent)),
+                          )
+                        ],
+                      ),
+                      // Container(
+                      //   margin: EdgeInsets.symmetric(vertical: kVPadding),
+                      //   alignment: Alignment.center,
+                      //   child: AdWidget(ad: myBanner),
+                      //   width: myBanner.size.width.toDouble(),
+                      //   height: myBanner.size.height.toDouble(),
+                      // ),
+                      SizedBox(
+                        height: kVPadding * 5,
+                      ),
+                      Text(
+                        'The more we share, the more we have',
+                        style: kAvertaTextStyle.copyWith(
+                          color: kSecondaryColor,
+                          fontSize: 14,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: kVPadding * 1.5,
+                      ),
+                      Text(
+                        '- Lionard Nimoy',
+                        style: kAvertaTextStyle.copyWith(
+                          color: kGrayColor,
+                          fontSize: 14,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: kVPadding * 6,
                       )
                     ],
-                  ),
-                  Container(
-                    alignment: Alignment.center,
-                    child: AdWidget(ad: myBanner),
-                    width: myBanner.size.width.toDouble(),
-                    height: myBanner.size.height.toDouble(),
-                  ),
-                  SizedBox(
-                    height: kVPadding * 5,
-                  ),
-                  Text(
-                    'The more we share, the more we have',
-                    style: kAvertaTextStyle.copyWith(
-                      color: kSecondaryColor,
-                      fontSize: 14,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: kVPadding * 1.5,
-                  ),
-                  Text(
-                    '- Lionard Nimoy',
-                    style: kAvertaTextStyle.copyWith(
-                      color: kGrayColor,
-                      fontSize: 14,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: kVPadding * 6,
-                  )
-                ],
-              ));
+                  ));
         });
   }
 
